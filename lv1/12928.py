30e174e8be46b0d7a056d21312aded2c7ebc7c07
myeonghan-nim <hannim0828@gmail.com>
2026-01-05 22:30:53 +0900

    feat: add 12928 solution in Python

---
+++ lv1/12928.py
@@ -0,0 +1,10 @@
+import math
+
+def solution(n):
+    if n < 2:
+        return n
+    answer = 1 + n
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            answer += i + (n // i if i != n // i else 0)
+    return answer
