360a2143a71127c0701e653ec89af5e7733b9576
myeonghan <52690495+myeonghan-nim@users.noreply.github.com>
2026-02-11 14:52:33 +0900

    feat: add 12945 solution

---
+++ lv2/12945.py
@@ -0,0 +1,13 @@
+def solution(n):
+    mod = 1234567
+
+    a, b = 0, 1
+    for bit in bin(n)[2:]:
+        c = (a * ((b << 1) - a)) % mod
+        d = (a * a + b * b) % mod
+        if bit == '0':
+            a, b = c, d
+        else:
+            a, b = d, (c + d) % mod
+
+    return a
