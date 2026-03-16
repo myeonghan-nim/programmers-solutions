991f5d71cbac2523d90240141ce57acb7fe1ec40
myeonghan-nim <hannim0828@gmail.com>
2026-03-16 21:50:43 +0900

    feat: add 43105 solution

---
+++ lv3/43105.py
@@ -0,0 +1,12 @@
+def solution(triangle):
+    n = len(triangle)
+
+    dp = [[0] * n for _ in range(n)]
+    dp[0][0] = triangle[0][0]
+    for i in range(1, n):
+        dp[i][0] = dp[i - 1][0] + triangle[i][0]
+        for j in range(1, i):
+            dp[i][j] = max(dp[i - 1][j - 1], dp[i - 1][j]) + triangle[i][j]
+        dp[i][i] = dp[i - 1][i - 1] + triangle[i][i]
+
+    return max(dp[n - 1])
