4c773eb02c5f2b136be92a225c77331daa11476f
myeonghan-nim <hannim0828@gmail.com>
2026-05-29 14:47:20 +0900

    feat: add 12907 solution

---
+++ lv3/12907.py
@@ -0,0 +1,11 @@
+def solution(n, money):
+    MOD = 1000000007
+
+    dp = [0] * (n + 1)
+    dp[0] = 1
+
+    for coin in money:
+        for i in range(coin, n + 1):
+            dp[i] = (dp[i] + dp[i - coin]) % MOD
+
+    return dp[n]
