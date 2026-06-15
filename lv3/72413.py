286dd146a456d72230e0e8f7611c4ed299342330
myeonghan <52690495+myeonghan-nim@users.noreply.github.com>
2026-06-15 13:48:07 +0900

    feat: add 72413 solution

---
+++ lv3/72413.py
@@ -0,0 +1,21 @@
+def solution(n, s, a, b, fares):
+    answer = float('inf')
+    graph = [[float('inf')] * (n + 1) for _ in range(n + 1)]
+
+    for i in range(1, n + 1):
+        graph[i][i] = 0
+
+    for fare in fares:
+        c, d, f = fare
+        graph[c][d] = f
+        graph[d][c] = f
+
+    for k in range(1, n + 1):
+        for i in range(1, n + 1):
+            for j in range(1, n + 1):
+                graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+
+    for i in range(1, n + 1):
+        answer = min(answer, graph[s][i] + graph[i][a] + graph[i][b])
+
+    return answer
